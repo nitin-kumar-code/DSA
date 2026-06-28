bfb3f059c9a026873bcf110284397d5be508fe81
Nitin Kumar <itsnitinkumar3000@gmail.com>
2026-06-28 19:26:00 +0530

    Binary search Questions

--- Arrays/Basic Problems.java
+++ Arrays/Basic Problems.java
@@ -23,92 +23,27 @@ class Solution {
 
 */
 
-/* 
-LeetCode: 34. Find First and Last Position of Element in Sorted Array
+/*
+LeetCode: 754. Reach a Number
 
 Difficulty: Medium
 
-Pattern: Arrays and Binary Search
+Pattern: Arrays and Math
 
 class Solution {
-    public int[] searchRange(int[] nums, int target) {
-        int[] ans = {-1, -1};
+    public int reachNumber(int target) {
 
-        ans[0] = search(nums, target, true);
+        target = Math.abs(target);
 
-        if (ans[0] != -1) {
-            ans[1] = search(nums, target, false);
+        int sum = 0;
+        int steps = 0;
+
+        while (sum < target || (sum - target) % 2 != 0) {
+            steps++;
+            sum += steps;
         }
 
-        return ans;
-    }
-
-    int search(int[] nums, int target, boolean findStartIndex) {
-        int ans = -1;
-        int start = 0;
-        int end = nums.length - 1;
-
-        while (start <= end) {
-            int mid = start + (end - start) / 2;
-
-            if (target < nums[mid]) {
-                end = mid - 1;
-            } else if (target > nums[mid]) {
-                start = mid + 1;
-            } else {
-                ans = mid;
-
-                if (findStartIndex) {
-                    end = mid - 1;   
-                } else {
-                    start = mid + 1; 
-                }
-            }
-        }
-
-        return ans;
-    }
-}
-*/
-
-/* 
-LeetCode: 33. Search in Rotated Sorted Array
-
-Difficulty: Medium
-
-Topic: Arrays and Binary Search
-
-class Solution {
-    public int search(int[] nums, int target) {
-        int low = 0;
-        int high = nums.length - 1;
-
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-
-            if (nums[mid] == target) {
-                return mid;
-            }
-
-            // Left half is sorted
-            if (nums[low] <= nums[mid]) {
-                if (nums[low] <= target && target < nums[mid]) {
-                    high = mid - 1;
-                } else {
-                    low = mid + 1;
-                }
-            }
-            // Right half is sorted
-            else {
-                if (nums[mid] < target && target <= nums[high]) {
-                    low = mid + 1;
-                } else {
-                    high = mid - 1;
-                }
-            }
-        }
-
-        return -1;
+        return steps;
     }
 }
 */
