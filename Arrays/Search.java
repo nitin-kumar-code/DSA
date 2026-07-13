c627e8782aaae8088cfee9c7f9af06b50c1b7cd5
Nitin Kumar <itsnitinkumar3000@gmail.com>
2026-07-13 11:31:01 +0530

    Add Solution for LC 540

--- Arrays/Search.java
+++ Arrays/Search.java
@@ -87,3 +87,36 @@ class Solution {
     }
 }
 */
+
+/*
+LeetCode: 540. Single Element in a Sorted Array
+
+Difficulty: Medium
+
+Topic: Arrays and Binary Search
+
+class Solution {
+    public int singleNonDuplicate(int[] nums) {
+
+        int start = 0;
+        int end = nums.length - 1;
+
+        while (start < end) {
+
+            int mid = start + (end - start) / 2;
+
+            if (mid % 2 == 1) {
+                mid--;
+            }
+
+            if (nums[mid] == nums[mid + 1]) {
+                start = mid + 2;
+            } else {
+                end = mid;
+            }
+        }
+
+        return nums[start];
+    }
+}
+*/
