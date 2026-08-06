d3ed2a2c7c5cb3543dbe75bb329a78357af31109
Nitin Kumar <itsnitinkumar3000@gmail.com>
2026-08-06 21:07:56 +0530

    Add Solution of LC 49.

--- HashMaps/Questions.java
+++ HashMaps/Questions.java
@@ -173,3 +173,33 @@ class Solution {
     }
 }
 */
+
+/*
+Leetcode: 49. Group Anagrams
+
+Difficulty: Medium
+
+Pattern: HashTable
+
+import java.util.*;
+
+class Solution {
+
+    public List<List<String>> groupAnagrams(String[] strs) {
+
+        HashMap<String, List<String>> map = new HashMap<>();
+
+        for (String word : strs) {
+            char[] ch = word.toCharArray();
+            Arrays.sort(ch);
+            String sortedWord = new String(ch);
+
+            if (!map.containsKey(sortedWord)) {
+                map.put(sortedWord, new ArrayList<>());
+            }
+            map.get(sortedWord).add(word);
+        }
+        return new ArrayList<>(map.values());
+    }
+}
+*/
