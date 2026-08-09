16b35a6d4ce222b429cd9b1c6b7bdc6aa46dc6d2
Nitin Kumar <itsnitinkumar3000@gmail.com>
2026-08-09 09:33:21 +0530

    Add Solutions of Two Knights and Tower of Hanoi

--- Arrays/Advanced Problems.java
+++ Arrays/Advanced Problems.java
@@ -40,3 +40,73 @@ public class NumberSpiral {
     }
 }
 */
+
+/*
+CSES: Two Knights
+
+Difficulty: Medium
+
+Pattern: Math + Combinatorics
+
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        for (int k = 1; k <= n; k++) {
+
+            long total = (long) k * k * (k * k - 1) / 2;
+
+            long attacking = 4L * (k - 1) * (k - 2);
+
+            long answer = total - attacking;
+
+            System.out.println(answer);
+        }
+    }
+}
+*/
+
+/*
+CSES: Tower of Hanoi
+
+Difficulty: Easy
+
+Pattern: Recursion
+
+import java.util.*;
+
+public class Main {
+
+    static void hanoi(int n, int from, int to, int aux) {
+
+        if (n == 0) {
+            return;
+        }
+
+        hanoi(n - 1, from, aux, to);
+
+        System.out.println(from + " " + to);
+
+        hanoi(n - 1, aux, to, from);
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        long moves = (1L << n) - 1;
+
+        System.out.println(moves);
+
+        hanoi(n, 1, 3, 2);
+    }
+}
+*/
