2ab0a1e46ce473aa3f3edf93e88e4a7d8726b41a
Nitin Kumar <itsnitinkumar3000@gmail.com>
2026-08-26 11:15:27 +0530

    Add Solution of LC 142.

---
+++ LinkedList/Questions.java
@@ -0,0 +1,35 @@
+/*
+  LC: 142. Linked List Cycle II
+
+  Difficulty: Medium
+
+  Pattern: Linked List + Two Pointers
+
+  public class Solution {
+    public ListNode detectCycle(ListNode head) {
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (slow == fast) {
+                slow = head;
+                while (slow != fast) {
+                    slow = slow.next;
+                    fast = fast.next;
+                }
+                return slow;
+            }
+        }
+        return null;
+    }
+}
+*/
+
+/*
+  LC: 
+*/
